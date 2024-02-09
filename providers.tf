9447a1b86c32e05b8290ec1eaff95a6afc4b0888
Emanuel Prado Macat <emanuel.prado@promedon.com>
2024-02-09 07:54:19 -0300

    commit inicial

---
+++ providers.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=4.36.0, <4.47.0, !=4.43.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = "~>1.6.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  assume_role {
+    role_arn     = "arn:aws:iam::189494931186:role/Sandbox_AdministratorAccess_Role"
+    session_name = "Sandbox"
+  }
+  default_tags {
+    tags = var.tags
+  }
+}
+
+
+
+
+
